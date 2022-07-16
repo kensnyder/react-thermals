292faf1c9976afb2a8aee45c53d7901fe9115e9c
Ken Snyder <kendsnyder@gmail.com>
2022-07-16 16:50:12 -0600

    [main] use re-exports for actions/index.js

--- actions/index.js
+++ actions/index.js
@@ -1,22 +1,10 @@
-import { fieldAdder, fieldAdderSync } from './fieldAdder.js';
-import { fieldAppender, fieldAppenderSync } from './fieldAppender.js';
-import { fieldListSetter, fieldListSetterSync } from './fieldListSetter.js';
-import { fieldRemover, fieldRemoverSync } from './fieldRemover.js';
-import { fieldSetter, fieldSetterSync, fieldSetterInput } from 'fieldSetter.js';
-import { fieldToggler, fieldTogglerSync } from './fieldToggler.js';
-
+export { fieldAdder, fieldAdderSync } from './fieldAdder.js';
+export { fieldAppender, fieldAppenderSync } from './fieldAppender.js';
+export { fieldListSetter, fieldListSetterSync } from './fieldListSetter.js';
+export { fieldRemover, fieldRemoverSync } from './fieldRemover.js';
 export {
-  fieldAdder,
-  fieldAdderSync,
-  fieldAppender,
-  fieldAppenderSync,
-  fieldListSetter,
-  fieldListSetterSync,
-  fieldRemover,
-  fieldRemoverSync,
   fieldSetter,
   fieldSetterSync,
   fieldSetterInput,
-  fieldToggler,
-  fieldTogglerSync,
-};
+} from './fieldSetter.js';
+export { fieldToggler, fieldTogglerSync } from './fieldToggler.js';
