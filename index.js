b2fc27ba94aff969ed1083c972ee9f761293b6d7
Ken Snyder <kendsnyder@gmail.com>
2022-11-03 21:38:08 -0600

    fix imports in index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-export * as Store from './src/Store/Store.js';
-export * as useStoreSelector from './src/useStoreSelector/useStoreSelector.js';
-export * as useStoreState from './src/useStoreState/useStoreState.js';
-export * as selectPath from './src/selectPath/selectPath.js';
-export * as shallowCopy from './src/shallowCopy/shallowCopy.js';
-export * as updatePath from './src/updatePath/updatePath.js';
+import Store from './src/Store/Store.js';
+import useStoreSelector from './src/useStoreSelector/useStoreSelector.js';
+import useStoreState from './src/useStoreState/useStoreState.js';
+export { Store, useStoreState, useStoreSelector };
