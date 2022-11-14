ef2f75b542387b91232e84abb0cdd3aad1035ec8
Ken Snyder <kendsnyder@gmail.com>
2022-11-14 09:28:16 -0700

    when testing emitter, use actual event names

--- src/classes/SimpleEmitter/SimpleEmitter.spec.ts
+++ src/classes/SimpleEmitter/SimpleEmitter.spec.ts
@@ -8,37 +8,37 @@ describe('SimpleEmitter', () => {
   });
   it('should be chainable', () => {
     const emitter = new SimpleEmitter();
-    const context = emitter.on('foo', () => {});
+    const context = emitter.on('BeforeInitialState', () => {});
     expect(context).toBe(emitter);
   });
   it('should return basic object from emit()', () => {
     const foo = {};
     const emitter = new SimpleEmitter();
-    const evt = emitter.emit('test', foo);
+    const evt = emitter.emit('BeforeSet', foo);
     expect(evt.data).toBe(foo);
-    expect(evt.type).toBe('test');
+    expect(evt.type).toBe('BeforeSet');
   });
   it('should return a Preventable event from emit()', () => {
     const foo = {};
     const emitter = new SimpleEmitter();
-    emitter.on('test', () => {});
-    const evt = emitter.emit('test', foo);
+    emitter.on('BeforeSet', () => {});
+    const evt = emitter.emit('BeforeSet', foo);
     expect(evt).toBeInstanceOf(PreventableEvent);
     expect(evt.target).toBe(emitter);
-    expect(evt.type).toBe('test');
+    expect(evt.type).toBe('BeforeSet');
   });
   it('should return the event from emit()', () => {
     const foo = {};
     const emitter = new SimpleEmitter();
-    const evt = emitter.emit('test', foo);
+    const evt = emitter.emit('BeforeSet', foo);
     expect(evt).toBeInstanceOf(Object);
     expect(evt.data).toBe(foo);
-    expect(evt.type).toBe('test');
+    expect(evt.type).toBe('BeforeSet');
   });
   it('should allow preventing default', () => {
     const emitter = new SimpleEmitter();
-    emitter.on('test', evt => evt.preventDefault());
-    const evt = emitter.emit('test');
+    emitter.on('BeforeSet', evt => evt.preventDefault());
+    const evt = emitter.emit('BeforeSet');
     expect(evt.defaultPrevented).toBe(true);
     expect(evt.isPropagationStopped()).toBe(false);
   });
@@ -46,26 +46,26 @@ describe('SimpleEmitter', () => {
     const emitter = new SimpleEmitter();
     let numCalls = 0;
     const handler = () => numCalls++;
-    emitter.on('test', handler);
-    emitter.off('test', handler);
-    emitter.emit('test');
+    emitter.on('BeforeSet', handler);
+    emitter.off('BeforeSet', handler);
+    emitter.emit('BeforeSet');
     expect(numCalls).toBe(0);
   });
   it('should support once()', () => {
     const emitter = new SimpleEmitter();
     let numCalls = 0;
     const handler = () => numCalls++;
-    emitter.once('test', handler);
-    emitter.emit('test');
+    emitter.once('BeforeSet', handler);
+    emitter.emit('BeforeSet');
     expect(numCalls).toBe(1);
-    emitter.emit('test');
+    emitter.emit('BeforeSet');
     expect(numCalls).toBe(1);
   });
   it('should do noop when calling off() without calling on() first', () => {
     const emitter = new SimpleEmitter();
     const remove = () => {
       const handler = () => {};
-      emitter.off('test', handler);
+      emitter.off('BeforeSet', handler);
     };
     expect(remove).not.toThrowError();
   });
@@ -73,8 +73,8 @@ describe('SimpleEmitter', () => {
     const emitter = new SimpleEmitter();
     let numCalls = 0;
     emitter.on('*', () => numCalls++);
-    emitter.emit('foo');
-    emitter.emit('bar');
+    emitter.emit('BeforeInitialState');
+    emitter.emit('BeforeUpdate');
     expect(numCalls).toBe(2);
   });
   it('should stop propagation', () => {
@@ -84,9 +84,9 @@ describe('SimpleEmitter', () => {
       numCalls++;
       evt.stopPropagation();
     };
-    emitter.on('test', handler);
-    emitter.on('test', handler);
-    const event = emitter.emit('test');
+    emitter.on('BeforeSet', handler);
+    emitter.on('BeforeSet', handler);
+    const event = emitter.emit('BeforeSet');
     expect(numCalls).toBe(1);
     expect(event.isPropagationStopped()).toBe(true);
   });
@@ -97,9 +97,9 @@ describe('SimpleEmitter', () => {
       numCalls++;
       evt.stopImmediatePropagation();
     };
-    emitter.on('test', handler);
-    emitter.on('test', handler);
-    const event = emitter.emit('test');
+    emitter.on('BeforeSet', handler);
+    emitter.on('BeforeSet', handler);
+    const event = emitter.emit('BeforeSet');
     expect(numCalls).toBe(1);
     expect(event.isPropagationStopped()).toBe(true);
   });
